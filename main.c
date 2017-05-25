1a1b038481f972087dee36b17f76e692fd9a470e
Theo Morales <theo.morales.fr@gmail.com>
2017-05-25 17:15:43 +0200

    Init

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main(int argc, char* argv[])
+{
+	int fd;
+	ssize_t ret_in, ret_out;
+	char buffer[1024];
+
+	fd = open("/dev/ttyUSB0", O_RDWR);
+
+	if (fd == -1) {
+		perror("open");
+
+		return 2;
+	}
+
+	while ((ret_in = read(fd, &buffer, 1024)) > 0) {
+		printf("%s", buffer);
+	}
+
+	close(fd);
+
+	return 0;
+}
