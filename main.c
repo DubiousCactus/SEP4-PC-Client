4f2cf2d5bfed7449e50fd85657452eb9142d244a
Theo Morales <theo.morales.fr@gmail.com>
2017-05-25 17:50:46 +0200

    Implement rudimental game loop

--- main.c
+++ main.c
@@ -16,14 +16,31 @@
 
 #endif
 
+int fd;
+
+void game_loop()
+{
+	char key = 0;
+	
+	printf("Starting game loop...\n\n");
+	
+	/* use system call to make terminal send all keystrokes directly to stdin */
+	system ("/bin/stty raw");
+
+	while ((key = getchar()) != '.') {
+
+		if (write(fd, &key, 1) > 0)
+			printf("Sent %c", key);
+		else
+			printf("Error sending %c", key);	
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	int fd;
 	ssize_t ret_in, ret_out;
 	char buffer[1024];
 
-	#ifndef OS_Windows
 	fd = open("/dev/ttyUSB0", O_RDWR);
 
 	if (fd == -1) {
@@ -32,12 +49,18 @@ int main(int argc, char* argv[])
 		return 2;
 	}
 
-	while ((ret_in = read(fd, &buffer, 1024)) > 0) {
-		printf("%s", buffer);
+	if (write(fd, "hello", 5) > 0) {
+		if (read(fd, &buffer, 1024) > 0) {
+			printf("Received: %s\n\n", buffer);
+			game_loop();
+		} else {
+			printf("Game controller didn't respond !");
+		}
+	} else {
+		printf("Error sending \"hello\"");
 	}
-
+	
 	close(fd);
-	#endif
 
 	return 0;
 }
